59a2b6560500c5c6723390af692af965988e6fd3
JAYBLA <jayblaenterprises@gmail.com>
2022-03-07 11:45:13 +0300

    replaced with CDN's

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SOILA | <?= $title ?> </title>
     <link rel="shortcut icon" href="images/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/css/bootstrap.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prettyPhoto/3.1.6/css/prettyPhoto.min.css" integrity="sha512-aB3UdGCt+QZdSlPCgDsJBJ+JytRb8oq/cdMEpLTaypINSyom0h5vcw2HsF1m0eZtWsetJllPtQOfCPM9UrdKYw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/revolution/css/rs6.css" />
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/css/shortcodes.css" />
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/css/main.css" />
@@ -16,10 +17,8 @@
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/css/megamenu.css" />
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/css/responsive.css" />
     <link rel="stylesheet" href="<?= base_url() ?>/css/animate.css">
-    <link rel="stylesheet" href="<?= base_url() ?>/css/font-awesome.css">
-    <link rel="stylesheet" href="<?= base_url() ?>/css/flaticon.css">
-    <link rel="stylesheet" href="<?= base_url() ?>/css/prettyPhoto.css">
-    <!-- <link rel="stylesheet" href="css/slick.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" integrity="sha512-4uGZHpbDliNxiAv/QzZNo/yb2FtAX+qiDb7ypBWiEdJQX8Pugp8M6il5SRkN8jQrDLWsh3rrPDSXRf3DwFYM6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prettyPhoto/3.1.6/css/prettyPhoto.min.css" integrity="sha512-aB3UdGCt+QZdSlPCgDsJBJ+JytRb8oq/cdMEpLTaypINSyom0h5vcw2HsF1m0eZtWsetJllPtQOfCPM9UrdKYw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= base_url() ?>/css/themify-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jQuery.mmenu/5.3.4/css/jquery.mmenu.all.min.css'>
@@ -47,17 +46,12 @@
         <!--back-to-top end-->
     </div><!-- page end -->
     <!-- Javascript -->
-    <script src="<?= base_url() ?>/js/jquery-3.6.0.min.js"></script>
-    <script src="<?= base_url() ?>/js/jquery-migrate-3.3.2.min.js"></script>
-    <script src="<?= base_url() ?>/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= base_url() ?>/js/jquery.easing.js"></script>
-    <script src="<?= base_url() ?>/js/jquery-waypoints.js"></script>
-    <script src="<?= base_url() ?>/js/jquery-validate.js"></script>
-    <script src="<?= base_url() ?>/js/jquery.prettyPhoto.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettyPhoto/3.1.6/js/jquery.prettyPhoto.min.js" integrity="sha512-IB7NSySDRedVEsYsOVuzN5O5jwRjV2ewVVmkDFIgE0yNu11GreBCOMv07i7hlQck41T+sTXSL05/cG+De4cZDw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!-- <script src="js/slick.min.js"></script> -->
     <script src="<?= base_url() ?>/js/numinate.min.js"></script>
-    <script src="<?= base_url() ?>/js/imagesloaded.min.js"></script>
-    <script src="<?= base_url() ?>/js/jquery-isotope.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/5.0.0/imagesloaded.min.js" integrity="sha512-GNb4Gyn5ZK+XcsnKg8jeo5olNYWqH+j+gMvbG1X5Z9Vz7bv/tQ2spLUuKcXo8T3r4p3IxVt0e/ARNwFftqCt+w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="<?= base_url() ?>/revolution/js/rbtools.min.js"></script>
     <script src="<?= base_url() ?>/revolution/js/rs6.min.js"></script>
     <script src="<?= base_url() ?>/revolution/js/slider.js"></script>
