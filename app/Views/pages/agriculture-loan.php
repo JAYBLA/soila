3e9ff8c21de77c39c92ac2f8049d54c715a155a5
JAYBLA <jayblaenterprises@gmail.com>
2022-04-09 16:37:11 +0300

    agriculture loan

--- app/Views/pages/agriculture-loan.php
+++ app/Views/pages/agriculture-loan.php
@@ -39,13 +39,101 @@
                     <article class="post ttm-blog-single clearfix">
                         <div class="ttm-blog-single-content mb-0">
                             <div class="entry-content">
-                                <h3>SOILA SACCOS</h3>
-                                <p>Our Saccos are based on provide agriculture loans to our members, you can be member
-                                    by joining through filling the forms anywhere and return it to our office through
-                                    email, on hand etc.
+                                <h3>GREENHOUSE AND DRIP IRRIGATION LOAN</h3>
+                                <p>Under agreement financing products agribusiness entrepreneurs are empowered to
+                                    acquire full installed greenhouse, drip irrigation system, training on greenhouse
+                                    management, hybrid seeds, soil test, chemicals and fertilizers under installment
+                                    payment, which would not have been able to acquire, anywhere. <br>
+                                    The entrepreneur will obtain the service without paying full price. But the
+                                    entrepreneur continues paying for the installed greenhouse or drip irrigation
+                                    systems in a number of agreed periodic installments. <br>
+                                    This product and services are introduced to help entrepreneurs acquire greenhouse
+                                    and drip irrigation systems for their enterprises while they do not have sufficient
+                                    capital to invest in such greenhouses and drip irrigation systems.
+
                                 </p>
+                                <h5>Features and Benefits of greenhouse and Drip irrigation Loans</h5>
+                                <p>The key features and benefits of horticulture loans are as follows:
+                                <ul class="ttm-list ttm-list-style-icon d-grid ttm-list-icon-color-skincolor">
+                                    <li> <i class="fa fa-arrow-circle-right"></i><span
+                                            class="ttm-list-li-content">Simplified Documentation: When borrowing a
+                                            greenhouse and drip irrigation loan,
+                                            applicants will only have to submit a few documents such as a valid photo
+                                            identity
+                                            proof, proof of residence, etc. These documents can be submitted along with
+                                            one’s
+                                            application form.</span></li>
+                                    <li><i class="fa fa-arrow-circle-right"></i><span class="ttm-list-li-content"> Quick
+                                            Processing: SOILA PLC officers will verify your application form and the
+                                            documents submitted by you, after which your loan application will be
+                                            approved.</span></li>
+                                    <li><i class="fa fa-arrow-circle-right"></i><span class="ttm-list-li-content"> No
+                                            interest rate: SOILA PLC has no interest rate in their service instead of
+                                            repaying the cost of greenhouse and drip irrigation through
+                                            installment.</span></li>
+                                    <li><i class="fa fa-arrow-circle-right"></i><span class="ttm-list-li-content"> Loan
+                                            Repayment Tenure Options: Our loan tenure is a mid -short term, from 6 month
+                                            to 12 months</span></li>
+                                    <li><i class="fa fa-arrow-circle-right"></i><span class="ttm-list-li-content"> No
+                                            Hidden Charges: When you borrow greenhouse and drip irrigation loan, you may
+                                            need to pay other charges such as the application form, processing fee, 75%
+                                            of total
+                                            loan, etc. However, SOILA PLC will usually inform prospective borrowers
+                                            about the
+                                            various charges that they may have to pay for before the greenhouse and drip
+                                            irrigation loan are borrowed.</span></li>
+
+                                </ul>
+                                </p>
+                                <h5>Services application procedures</h5>
+                                <h6>1. Who can apply?</h6>
+                                <p>All individuals, groups, cooperatives, micro, small and medium size private
+                                    agribusiness entrepreneurs can apply for SOILA PLC products and services.</p>
+                                <h6>2. Procedures and documents</h6>
+                                <p>Clients may apply for SOILA PLC products and service directly from Head Office or
+                                    through a downloading form in our website(<a
+                                        href="https://soila.co.tz">www.soila.co.tz</a> ). <br>
+                                    The following procedures and documents apply in the process:
+                                <ol>
+                                    <li>Fill and submit application form</li>
+                                    <li>Pay application fees</li>
+                                    <li>Pre-evaluation review by SOILA PLC Officer</li>
+                                    <li>Pre-evaluation site visit by SOILA PLC Officer</li>
+                                    <li>Business plan</li>
+                                    <li>Bank Pay in slip of 75% of your requested loan</li>
+                                    <li>Sponsors/guarantor documents</li>
+                                    <li>Village/ street chairman letter for verification of your residence</li>
+                                    <li>Proposal evaluation by SOILA PLC Officer</li>
+                                    <li>Proposal evaluation by SOILA PLC during loan processing</li>
+                                    <li>Any other document mandated by the SOILA PLC</li>
+
+                                </ol>
+                                </p>
+                                <h6>3. Application Forms</h6>
+                                <p>Clients may apply for SOILA PLC products and service directly from Head Office or
+                                through a downloading form below <br>
+                                <a href="<?= base_url() ?>/documents/agriculture-loan.pdf" download="SOILA-Agriculture-Loan-Application-Form"><strong style="font-size:18px;margin-top:20px;">Agriculture Loan- Application Form</strong></a> </p>
                             </div>
                     </article><!-- post end -->
+                </div>
+                <div class="col-lg-12 content-area mt-5">
+                <h4>Greenhouse and Drip irrigation Loan Frequently Asked Questions</h4>
+                                <div class="accordion">
+                                        <!-- toggle -->
+                                        <div class="toggle ttm-toggle_style_classic style3 ttm-toggle-title-bgcolor-white">
+                                            <div class="toggle-title"><a href="#" class="active">How long do lenders take to process greenhouse and drip irrigation loan applications?</a></div>
+                                            <div class="toggle-content show">
+                                                <p>The time required to process your application will vary based on the SOILA PLC terms and conditions. It will take around 7 days to process loan applications.</p>
+                                            </div>
+                                        </div><!-- toggle end -->
+                                        <!-- toggle -->
+                                        <div class="toggle ttm-toggle_style_classic style3 ttm-toggle-title-bgcolor-white">
+                                            <div class="toggle-title"><a href="#">Will I need a guarantor to avail a greenhouse and drip irrigation loan?</a></div>
+                                            <div class="toggle-content">
+                                                <p>Yes, SOILA PLC will require you to have a guarantor.</p>
+                                            </div>
+                                        </div><!-- toggle end -->
+                                </div>
                 </div>
             </div><!-- row end -->
         </div>
